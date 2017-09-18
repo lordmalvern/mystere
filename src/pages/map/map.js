8058e3f1098b08437b22083b1bc22c5eec307e31
Brian Puschell <bbpuschell@gmail.com>
2017-09-18 05:08:23 +0000

    Started work on map page

--- src/pages/map/map.js
+++ src/pages/map/map.js
@@ -7,20 +7,53 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require('@angular/core');
 var ionic_angular_1 = require('ionic-angular');
-/**
- * Generated class for the MapPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 var MapPage = (function () {
     function MapPage(navCtrl, navParams) {
         this.navCtrl = navCtrl;
         this.navParams = navParams;
+        this.dirService = new google.maps.DirectionsService;
+        this.dirDisplay = new google.maps.DirectionsRenderer;
+        this.dirRequest = new google.maps.DirectionsRequest;
     }
     MapPage.prototype.ionViewDidLoad = function () {
+        this.initMap();
         console.log('ionViewDidLoad MapPage');
     };
+    MapPage.prototype.initMap = function () {
+        this.map = new google.maps.Map(this.mapElement.nativeElement, {
+            zoom: 11,
+            center: { lat: 37.7831413, lng: -122.4029678 }
+        });
+        this.dirDisplay.setMap(this.map);
+    };
+    MapPage.prototype.addWp = function () {
+        wps: any = dirRequest.waypoints;
+        place: Checkbox;
+        if (!place.checked && !place.disabled) {
+            wps.push({
+                location: place.value,
+                stopover: true
+            });
+        }
+        else if (!place.disabled) {
+            wps.splice(wps.findIndex(function (e) { return e.location === place.value; }), 1);
+        }
+        dirRequest.waypoints = wps;
+        dirService.route(dirRequest, function (res, stat) {
+            if (stat === 'OK') {
+                dirDisplay.setDirections(res);
+            }
+        }, private, route, DirectionsRoute = res.routes[0]);
+    };
+    MapPage.prototype.for = function (let, i, i) {
+        if (let === void 0) { let = i = 0; }
+        if (i === void 0) { i = ; }
+        if (i === void 0) { i = ++; }
+        totalSec += route.legs[i].duration.value;
+    };
+    __decorate([
+        core_1.ViewChild('map')
+    ], MapPage.prototype, "mapElement");
     MapPage = __decorate([
         ionic_angular_1.IonicPage(),
         core_1.Component({
@@ -31,3 +64,4 @@ var MapPage = (function () {
     return MapPage;
 }());
 exports.MapPage = MapPage;
+;
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -8,18 +8,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare var google;
+
 @IonicPage()
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class MapPage {
-
+  
+  @ViewChild('map') mapElement: ElementRef;
+  private map: any;
+  private dirService: DirectionsService = new google.maps.DirectionsService;
+  private dirDisplay: DirectionsRenderer = new google.maps.DirectionsRenderer;
+  private dirRequest: DirectionsRequest = new google.maps.DirectionsRequest;
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+    this.initMap();
     console.log('ionViewDidLoad MapPage');
   }
-
+  
+  initMap() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 11,
+      center: {lat: 37.7831413, lng: -122.4029678}
+    });
+    this.dirDisplay.setMap(this.map);
+  }
+  
+  addWp() {
+    wps: any = dirRequest.waypoints;
+    @ViewChild('place') place: Checkbox; 
+    if (!place.checked && !place.disabled) {
+    wps.push({
+      location: place.value,
+      stopover: true
+    });
+    } else if (!place.disabled) {
+      wps.splice(wps.findIndex((e) => e.location === place.value), 1);
+    }
+    dirRequest.waypoints = wps;
+    dirService.route(dirRequest, (res, stat) => {
+      if (stat === 'OK') {
+        dirDisplay.setDirections(res);
+        private route: DirectionsRoute = res.routes[0];
+        private totalSec: Number;
+        for (let i = 0; i < route.legs.length; i++) {
+          totalSec += route.legs[i].duration.value;
+        }
+        
+      }
+    });
+  }
 }
